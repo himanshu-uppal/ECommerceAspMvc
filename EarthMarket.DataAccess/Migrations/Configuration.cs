fa5a1bed234d0344d25d41a565b57fcf44825533
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-12-13 17:21:36 +0530

    updated images and updated connection string

--- EarthMarket.DataAccess/Migrations/Configuration.cs
+++ EarthMarket.DataAccess/Migrations/Configuration.cs
@@ -557,7 +557,9 @@
 
             List<string> productVariantImagesStrings = new List<string>();
 
-            productVariantImagesStrings.AddRange(new List<string> { "1", "2", "3", "4", "5", "6", "7", "8" });
+            productVariantImagesStrings.AddRange(new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "11", "12", "13", "14", "15", "16", "17","18","19" ,"20",
+                "21","22","23","24","25","26","27","28","29","30" });
 
             List<ProductImage> productVariantImages = new List<ProductImage>();
 
