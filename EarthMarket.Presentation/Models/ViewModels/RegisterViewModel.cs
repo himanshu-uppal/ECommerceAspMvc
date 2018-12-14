2c0ada055311698b69193dc4d32eb18087a8d6d3
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-12-14 10:51:37 +0530

    Changed validation for email in view model

--- EarthMarket.Presentation/Models/ViewModels/RegisterViewModel.cs
+++ EarthMarket.Presentation/Models/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using EarthMarket.Shared.DTOs;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +9,25 @@ using System.Web.Mvc;
 namespace EarthMarket.Presentation.Models.ViewModels
 {
     public class RegisterViewModel
-    {        
+    {
+
+
+        [Required(ErrorMessage = "Please provide the User name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please provide the Email")]
+        [MaxLength(50)]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string ConfirmPassword { get; set; }
+        [Required]
         public IEnumerable<string> Roles { get; set; }
+
+      
 
         public RegisterViewModel()
         {
